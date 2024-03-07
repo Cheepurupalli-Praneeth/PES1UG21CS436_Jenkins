2473884f4907911932e3215c9e3113450a02b4a3
Ch Praneeth <117644616+Cheepurupalli-Praneeth@users.noreply.github.com>
2024-03-07 18:11:45 +0530

    Third commit

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,7 +5,8 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "My SRN is : PES1UG21CS436";
+  cout << "My SRN is : PES1UG21CS436\n";
+  cout << "My name is Praneeth Cheepurupalli\n";
   cout << "I have successfully built and run\n";
   return 0;
 }
