f8944c7167449e40d6539923ecfb654a13598a95
Ch Praneeth <117644616+Cheepurupalli-Praneeth@users.noreply.github.com>
2024-03-07 16:57:07 +0530

    second commit

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "My SRN is : PES1UG21CS436"
+  cout << "My SRN is : PES1UG21CS436";
   cout << "I have successfully built and run\n";
   return 0;
 }
